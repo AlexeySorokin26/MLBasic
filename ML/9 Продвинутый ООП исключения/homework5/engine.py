4bde8a65ca29b35b281d2029df989d0e524fb547
Alexey Sorokin <asorokin@gremse-it.com>
2025-11-30 19:28:54 +0100

    removed

--- ML/9 Продвинутый ООП исключения/homework5/engine.py
+++
@@ -1,8 +0,0 @@
-from dataclasses import dataclass
-
-# decorator dataclass automatically generate __init__, __repr__, __eq__...
-@dataclass
-class Engine:
-    volume: int
-    pistons: int
-    
--- ML/9 Продвинутый ООП исключения/homework5/exceptions.py
+++
@@ -1,6 +0,0 @@
-class LowFuelError(Exception):
-    pass
-class NotEnoughFuel(Exception):
-    pass
-class CargoOverLoad(Exception):
-    pass
